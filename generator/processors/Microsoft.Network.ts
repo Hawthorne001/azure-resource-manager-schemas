eaf993dfa78493d86c9d4a8c60bb03cbf5e5184c
Gokul Premraj <gopremra@microsoft.com>
2026-05-01 13:21:05 -0400

    Fix cyclic refs in NRP

--- generator/processors/Microsoft.Network.ts
+++ generator/processors/Microsoft.Network.ts
@@ -5,6 +5,7 @@ import { replaceCyclicRefByName } from './helpers';
 
 export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, schema) => {
   replaceCyclicRefByName(schema, 'PublicIPAddress', { type: 'object' });
+  replaceCyclicRefByName(schema, 'CommonPublicIPAddress', { type: 'object' });
   replaceCyclicRefByName(schema, 'NetworkInterface', { type: 'object' });
   replaceCyclicRefByName(schema, 'NetworkSecurityGroup', { type: 'object' });
   replaceCyclicRefByName(schema, 'RouteTable', { type: 'object' });
